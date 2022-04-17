03bc478395c2dfbb2368325d35e08d26df058904
TactLabs <raja.csp@gmail.com>
2022-04-17 11:54:47 -0400

    Basic JSON score integration done

--- app.py
+++ app.py
@@ -174,4 +174,8 @@ def get_flag():
     )
 
 if __name__== "__main__":
-    app.run(host="0.0.0.0", debug = True, port = 5003)
+    app.run(
+        host = "0.0.0.0", 
+        debug = True, 
+        port = 5003
+    )
